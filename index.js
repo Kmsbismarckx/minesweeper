eeece49eeb1290dbfb2463b730e5ce4843e2cdeb
Bismarck <klimentmaslennikov@gmail.com>
2022-08-03 23:27:14 +0300

    remove bug with eventListener for click

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 function startGame() {
   const level = document.querySelector('.level');
   const form = document.querySelector('form');
-  console.log('start')
-  setField(level.value);
+  setField(+level.value);
 
   level.addEventListener('change', () => {
     const field = document.querySelector('.field');
     field.classList.remove('win');
     field.classList.remove('lose');
-    setField(level.value);
-    console.log('set')
+    setField(+level.value);
   })
 
   form.addEventListener('submit', (event) => {
@@ -19,7 +17,6 @@ function startGame() {
 }
 
 function setField(size) {
-  size = +size;
   const mines = Math.round(size * 1.5);
   const field = document.querySelector('.field');
   const cellsCount = Math.pow(size, 2);
@@ -37,78 +34,78 @@ function setField(size) {
     .slice(0, mines)
     .map((item) => item + 1)
     .sort((a, b) => a - b);
-  console.log(bombs)
   const flags = [];
 
-  field.addEventListener('click', (event) => {
-    let cell = event.target;
 
-    if (cell.tagName !== 'BUTTON') return;
+  cells.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      let cell = event.target;
 
-    if (isBomb(cell.id)) {
-      isGameOver = true;
-      gameOver();
-      cells.forEach((item) => item.disabled = true);
-    } else {
-      cell.innerHTML = bombsAround(cell.id);
-      cell.disabled = true;
-    }
+      if (cell.tagName !== 'BUTTON') return;
 
-    if (gameWinCheck()) {
-      gameWin();
-    }
+      if (isBomb(+cell.id)) {
+        isGameOver = true;
+        gameOver();
+        cells.forEach((item) => item.disabled = true);
+      } else {
+        cell.innerHTML = bombsAround(+cell.id);
+        cell.disabled = true;
+      }
+
+      if (gameWinCheck()) {
+        gameWin();
+      }
+    })
+
+    button.addEventListener('contextmenu', (event) => {
+      let cell = event.target;
+
+      if (cell.tagName !== 'BUTTON' || cell.textContent !== '' || isGameOver || isGameWin) return;
+      event.preventDefault();
+
+      if (flags.includes(+cell.id)) {
+        cell.classList.remove('flag');
+        flags.splice(flags.indexOf(+cell.id), 1);
+        cell.disabled = false;
+      } else {
+        cell.classList.add('flag');
+        flags.push(+cell.id);
+        cell.disabled = true;
+      }
+
+      if (gameWinCheck()) {
+        gameWin();
+      }
+    })
   });
 
-  field.addEventListener('contextmenu', (event) => {
-    let cell = event.target;
-
-    if (cell.tagName !== 'BUTTON' || cell.textContent !== '' || isGameOver || isGameWin) return;
-    event.preventDefault();
-
-    if (flags.includes(+cell.id)) {
-      cell.classList.remove('flag');
-      flags.splice(flags.indexOf(+cell.id), 1);
-      cell.disabled = false;
-    } else {
-      cell.classList.add('flag');
-      flags.push(+cell.id);
-      cell.disabled = true;
-    }
-
-    if (gameWinCheck()) {
-      gameWin();
-    }
-  })
-
   function isBomb(index) {
-    return bombs.includes(+index);
+    return bombs.includes(index);
   }
 
   function bombsAround(index) {
-    console.log(`index ${index}`)
-    console.log(`size ${size}`)
     let count = 0;
     let aroundCells = [];
 
-    if (+index % +size === 1) {
+    if (index % size === 1) {
       aroundCells.push(
-        +index + 1,
-        +index - size, +index - (size - 1),
-        +index + size, +index + (size + 1)
+        index + 1,
+        index - size, index - (size - 1),
+        index + size, index + (size + 1)
       )
 
     }
-    else if (+index % +size === 0) {
+    else if (index % size === 0) {
       aroundCells.push(
-        +index - 1,
-        +index - (size + 1), +index - size,
-        +index + (size - 1), +index + size
+        index - 1,
+        index - (size + 1), index - size,
+        index + (size - 1), index + size
       )
     } else {
       aroundCells.push(
-        +index - 1, +index + 1,
-        +index - (size + 1), +index - size, +index - (size - 1),
-        +index + (size - 1), +index + size, +index + (size + 1)
+        index - 1, index + 1,
+        index - (size + 1), index - size, index - (size - 1),
+        index + (size - 1), index + size, index + (size + 1)
       )
     }
 
@@ -143,9 +140,8 @@ function setField(size) {
   }
 
   function gameWinCheck() {
-    return flags.length !== 0
-        && cells.every(item => item.disabled === true)
-        && bombs.sort().join('') === flags.sort().join('');
+    return cells.every(item => item.disabled === true)
+        && bombs.sort().join(',') === flags.sort().join(',');
   }
 
   function gameWin() {
