f99b7b1d6e02940becfbf243e1d92366764f2402
Bismarck <klimentmaslennikov@gmail.com>
2022-08-17 14:47:58 +0300

    add bombsAround

--- src/bombsAround/bombsAround.js
+++ src/bombsAround/bombsAround.js
@@ -1,6 +1,7 @@
 export function bombsAround(index, size, bombs) {
     let count = 0;
     let aroundCells = [];
+    let emptyAroundCells = new Set();
 
     if (index % size === 1) {
         aroundCells.push(
@@ -29,14 +30,18 @@ export function bombsAround(index, size, bombs) {
     for (let i = 0; i < aroundCells.length; i++) {
 
         for (let j = 0; j < bombs.length; j++) {
-
-            if (aroundCells[i] === bombs[j]) count++;
-
+            if (aroundCells[i] > 0 && aroundCells[i] <= size ** 2) {
+                if (aroundCells[i] === bombs[j]) {
+                    count++;
+                } else {
+                    emptyAroundCells.add(aroundCells[i])
+                }
+            }
         }
 
     }
     return {
-        aroundCells,
+        emptyAroundCells,
         count
     };
 }
