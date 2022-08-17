45d5265261311f1acec09166fe1f9ce8ca80b047
Bismarck <klimentmaslennikov@gmail.com>
2022-08-17 19:34:12 +0300

    add refresh for the game

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ import {setField} from "./src/setField/setField.js";
 function startGame() {
   const level = document.querySelector('.level');
   const form = document.querySelector('form');
-  setField(+level.value);
+  const options = document.querySelectorAll('option');
+  const field = document.querySelector('.field')
+  let cashedDifficulties = +localStorage.getItem('difficult');
+
+  Array.from(options).forEach(item => {
+    console.log(typeof cashedDifficulties)
+    item.selected = item.value === cashedDifficulties.toString();
+  })
+
+  setField(cashedDifficulties);
+
 
   level.addEventListener('change', () => {
     const field = document.querySelector('.field');
     field.classList.remove('win');
     field.classList.remove('lose');
     setField(+level.value);
+    localStorage.setItem('difficult', `${level.value}`)
   })
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
-    document.location.reload();
+    field.classList.remove('lose', 'win')
+    setField(+localStorage.getItem('difficult'))
   })
 }
 
